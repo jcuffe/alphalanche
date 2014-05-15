397e06adb1bd8195f2a9fd65fe8c6c968b1bff92
= <=>
2014-05-14 18:57:22 -0700

    implemented letter selection and word scoring

--- app/assets/javascripts/game.js
+++ app/assets/javascripts/game.js
@@ -9,8 +9,15 @@ $(function() {
     dictionary = $("#game-data").data("dictionary");
     letters = $("#game-data").data("letters");
 
+    // initialize array to hold current word
+    current_word = [];
+
+    score = 0;
+
     // function to add letters to a random square
     function add_letter(count) {
+
+        console.log("adding " + count + " letters");
        
         // find all empty cells
         cells = empty_cells();
@@ -26,6 +33,7 @@ $(function() {
 
             // assign letter to cell
             cell.data("letter", letter);
+            cell.data("active", "true");
             cell.html(letter);
             
             // game ends if the last cell has just been filled
@@ -56,23 +64,66 @@ $(function() {
         return;
     }
 
+    // adds a letter to the current_word array and updates the word display
+    function push_letter(letter) {
+        current_word.push(letter);
+        update_word();
+    }
+
+    // splices the first instance of the given letter from current_word array and updates the word display
+    function pop_letter(letter) {
+        current_word.splice(current_word.indexOf(letter), 1);
+        update_word();
+    }
+
+    // determine validity and value of current word and add to player's score
+    function score_word() {
+        // only score words 3 letters or longer
+        word_length = current_word.length;
+        if (word_length < 3) {
+            return;
+        }
+        score += word_length
+        $("#current-score").html(score);
+        $(".selected")
+            .removeClass("selected")
+            .data("letter", "")
+            .data("selected", "false")
+            .html("");
+        add_letter(word_length - 2);
+        current_word = [];
+        update_word();
+    }
+
+    function update_word() {
+        $("#current-word").html(current_word.join(""));
+    }
+
+    function cell_click() {
+        if ($(this).data("letter") != "") {
+            if ($(this).data("selected") == "true") {
+                $(this).removeClass("selected");
+                $(this).data("selected", "false");
+                pop_letter($(this).data("letter"));
+            } else {
+                $(this).addClass("selected");
+                $(this).data("selected", "true");
+                push_letter($(this).data("letter"));
+            }
+        }
+    }
+
     // calculate proper height for square cells, initialize data, and attach onclick handler
     $(".game-cell").css("height", $(".game-cell").css("width"));
-    $(".game-cell").click(function() {
-        if ($(this).data("selected") == "true") {
-            $(this).css("background-color", "#000");
-            pop_letter($(this).html)
-
-        } else {
-            $(this).css("background-color", "#444");
-        }
-    });
+    $(".game-cell").click(cell_click);
 
     // onclick handler for "pass turn" button
     $("#pass-turn").click(function() {
         add_letter(1);
     });
 
+    $("#score-word").click(score_word);
+
     // initialize game board with 6 letters
-    add_letter(6);
+    add_letter(10);
 });
